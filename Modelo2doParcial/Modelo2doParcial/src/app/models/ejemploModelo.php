a9dd6aa8b86acad0a52f30fe488d5686022e127e
mercedesguerrero <m.mercedesguerrero@gmail.com>
2019-09-29 23:12:34 -0300

    modelo parcial

--- Modelo2doParcial/Modelo2doParcial/src/app/models/ejemploModelo.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-class ejemploModelo extends \Illuminate\Database\Eloquent\Model
-{
-
-}
-?>
--- Modelo2doParcial/Modelo2doParcial/src/app/models/logueo.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-class logueo extends \Illuminate\Database\Eloquent\Model
-{
-
-}
-?>
